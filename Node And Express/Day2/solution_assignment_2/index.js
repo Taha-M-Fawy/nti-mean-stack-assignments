c7074d7d274c74faa7e58b280ba8f925ba8532fa
Taha-M-Fawy <tahafawy2@gmail.com>
2026-09-10 18:41:05 +0300

    updata Day2 Node assignment solution

--- Node And Express/Day2/solution_assignment_2/index.js
+++ Node And Express/Day2/solution_assignment_2/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const todosRouter = require("./src/routes/todosRouter.js");
+const todosRouter = require("./src/routes/todos.router.js");
 
 const app = express();
 const PORT = 3000;
